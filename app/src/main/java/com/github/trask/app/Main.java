0d40e999f3d99547e88da4996fa67eed1eaa0a3c
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-11-08 21:43:15 -0800

    Sleep after starting

--- app/src/main/java/com/github/trask/app/Main.java
+++ app/src/main/java/com/github/trask/app/Main.java
@@ -1,8 +1,11 @@
 package com.github.trask.app;
 
+import static java.util.concurrent.TimeUnit.HOURS;
+
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     System.out.println("hi from main!");
+    Thread.sleep(HOURS.toMillis(1));
   }
 }
